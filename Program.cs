7d8cddfafc77b3d37e360998501eb2938b16e07b
michaela <m.hunarwindhager@gmail.com>
2025-01-08 15:44:33 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics.Metrics;
+
+namespace TikTakToe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int GRID_SIZE_ROW = 3;
+            const int GRID_SIZE_COLUMN = 3;
+
+            char [,] grid = new char[GRID_SIZE_ROW, GRID_SIZE_COLUMN];
+           
+            for (int lineIndex = 0; lineIndex < GRID_SIZE_ROW; lineIndex++)
+            {
+                for (int columnIndex = 0; columnIndex < GRID_SIZE_COLUMN; columnIndex++)
+                {
+                    grid[lineIndex, columnIndex] = '_';
+                    Console.Write(grid[lineIndex, columnIndex] + " ");
+                    
+                }
+                Console.WriteLine();
+            }           
+            Console.WriteLine("Above you see tik tak toe");
+
+            string currentPlayer = "X";
+            if (currentPlayer == "X")
+            {
+                currentPlayer = "O";
+            }
+            else
+            {
+                currentPlayer = "X";
+            }
+
+            Console.WriteLine("Enter the row and column(e.g., 0 1):"); 
+            string move = Console.ReadLine(); 
+        }
+    }
+}
