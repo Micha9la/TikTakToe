5bd920b58f7b35d2faf665e742cc5da16bd3b32e
michaela <m.hunarwindhager@gmail.com>
2025-01-13 15:07:01 +0100

    added a column and row header for cell identification

--- Program.cs
+++ Program.cs
@@ -10,18 +10,25 @@ namespace TikTakToe
             const int GRID_SIZE_ROW = 3;
             const int GRID_SIZE_COLUMN = 3;
 
-            char [,] grid = new char[GRID_SIZE_ROW, GRID_SIZE_COLUMN];
-           
+            char[,] grid = new char[GRID_SIZE_ROW, GRID_SIZE_COLUMN];
+
+            Console.Write("  ");
+            for (int columnHeader = 0; columnHeader < GRID_SIZE_COLUMN; columnHeader++)
+            {
+                Console.Write(columnHeader + 1 + " ");
+            }
+            Console.WriteLine();
+            
             for (int lineIndex = 0; lineIndex < GRID_SIZE_ROW; lineIndex++)
             {
+                Console.Write(lineIndex + 1 + " ");
                 for (int columnIndex = 0; columnIndex < GRID_SIZE_COLUMN; columnIndex++)
                 {
                     grid[lineIndex, columnIndex] = '_';
                     Console.Write(grid[lineIndex, columnIndex] + " ");
-                    
                 }
                 Console.WriteLine();
-            }           
+            }
             Console.WriteLine("Above you see tik tak toe grid");
 
             string currentPlayer = "X";
@@ -34,8 +41,8 @@ namespace TikTakToe
                 currentPlayer = "X";
             }
 
-            Console.WriteLine("Enter the row and column(e.g., 0 1):"); 
-            string move = Console.ReadLine(); 
+            Console.WriteLine("Enter the row and column(e.g., 0 1):");
+            string move = Console.ReadLine();
         }
     }
 }
