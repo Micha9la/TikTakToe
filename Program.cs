658d67a614400122577ead9d3f34c4300fbc8aba
michaela <m.hunarwindhager@gmail.com>
2025-01-16 10:26:55 +0100

    Grid updated showing the second move by player O

--- Program.cs
+++ Program.cs
@@ -43,12 +43,12 @@ namespace TikTakToe
             }
 
             Console.WriteLine("Enter the row and column(e.g., 11 for the very first cell):");
-            string move = Console.ReadLine();
-            Console.WriteLine("You picked cell " + move);
-            int row = int.Parse(move[0].ToString()) - 1;
-            int col = int.Parse(move[1].ToString()) - 1;
-            grid[row, col] = 'X';
-            Console.WriteLine(grid[row, col]);
+            string moveUserX = Console.ReadLine();
+            Console.WriteLine("You picked cell " + moveUserX);
+            int rowX = int.Parse(moveUserX[0].ToString()) - 1;
+            int colX = int.Parse(moveUserX[1].ToString()) - 1;
+            grid[rowX, colX] = 'X';
+            //Console.WriteLine(grid[rowX, colX]); = displays only X
 
             Console.Write("  ");
             for (int columnHeader = 0; columnHeader < GRID_SIZE_COLUMN; columnHeader++)
@@ -69,7 +69,32 @@ namespace TikTakToe
             }
             Console.WriteLine("Above you see the updated tik tak toe grid");
 
+            Console.WriteLine("Enter the row and column(e.g., 11 for the very first cell):");
+            string moveUserO = Console.ReadLine();
+            Console.WriteLine("You picked cell " + moveUserO);
+            int rowO = int.Parse(moveUserO[0].ToString()) - 1;
+            int colO = int.Parse(moveUserO[1].ToString()) - 1;
+            grid[rowO, colO] = 'O';
+            //Console.WriteLine(grid[rowX, colX]); = displays only X
 
+            Console.Write("  ");
+            for (int columnHeader = 0; columnHeader < GRID_SIZE_COLUMN; columnHeader++)
+            {
+                Console.Write(columnHeader + 1 + " ");
+            }
+            Console.WriteLine();
+
+            for (int lineIndex = 0; lineIndex < GRID_SIZE_ROW; lineIndex++)
+            {
+                Console.Write(lineIndex + 1 + " ");
+                for (int columnIndex = 0; columnIndex < GRID_SIZE_COLUMN; columnIndex++)
+                {
+
+                    Console.Write(grid[lineIndex, columnIndex] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("Above you see the updated tik tak toe grid");
         }
     }
 }
