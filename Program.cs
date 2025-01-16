eda84d1b1665846d4f1173258290f2f922f0c743
michaela <m.hunarwindhager@gmail.com>
2025-01-16 09:43:42 +0100

    Grid updated showing the first move by player X

--- Program.cs
+++ Program.cs
@@ -48,7 +48,7 @@ namespace TikTakToe
             int row = int.Parse(move[0].ToString()) - 1;
             int col = int.Parse(move[1].ToString()) - 1;
             grid[row, col] = 'X';
-
+            Console.WriteLine(grid[row, col]);
 
             Console.Write("  ");
             for (int columnHeader = 0; columnHeader < GRID_SIZE_COLUMN; columnHeader++)
@@ -56,16 +56,20 @@ namespace TikTakToe
                 Console.Write(columnHeader + 1 + " ");
             }
             Console.WriteLine();
+
             for (int lineIndex = 0; lineIndex < GRID_SIZE_ROW; lineIndex++)
             {
                 Console.Write(lineIndex + 1 + " ");
                 for (int columnIndex = 0; columnIndex < GRID_SIZE_COLUMN; columnIndex++)
                 {
-                    grid[lineIndex, columnIndex] = '_';
+                    
                     Console.Write(grid[lineIndex, columnIndex] + " ");
                 }
                 Console.WriteLine();
             }
+            Console.WriteLine("Above you see the updated tik tak toe grid");
+
+
         }
     }
 }
