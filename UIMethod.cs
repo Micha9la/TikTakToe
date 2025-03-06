df6318e5caa554650d8d76285c757e28017bd702
michaela <m.hunarwindhager@gmail.com>
2025-03-06 12:49:30 +0100

    added comments just to explain code

--- UIMethod.cs
+++ UIMethod.cs
@@ -11,22 +11,24 @@ namespace TikTakToe
     {        
         public static void DisplayGrid(char[,] grid)
         {
+            //prints 2 spaces of alignment
             Console.Write("  ");
+            //Prints column numbers (1 2 3) so users see coordinates.
             for (int columnHeader = 0; columnHeader < Constants.GRID_SIZE_COLUMN; columnHeader++)
             {
-                Console.Write(columnHeader + 1 + " ");
+                Console.Write(columnHeader + 1 + " "); //We add 1 because arrays are zero-based, but users see 1-based indexing.
             }
-            Console.WriteLine();
+            Console.WriteLine(); //moves to a new line
 
-            for (int lineIndex = 0; lineIndex < Constants.GRID_SIZE_ROW; lineIndex++)
+            for (int lineIndex = 0; lineIndex < Constants.GRID_SIZE_ROW; lineIndex++)//loops through rows
             {
-                Console.Write(lineIndex + 1 + " ");
+                Console.Write(lineIndex + 1 + " ");//prints row numbers
                 for (int columnIndex = 0; columnIndex < Constants.GRID_SIZE_COLUMN; columnIndex++)
                 {
                     //grid[lineIndex, columnIndex] = '_'; if this is gone it will display the respective sign X at the spot user picked
-                    Console.Write(grid[lineIndex, columnIndex] + " ");
+                    Console.Write(grid[lineIndex, columnIndex] + " ");//Prints X, O, or empty space
                 }
-                Console.WriteLine();
+                Console.WriteLine();//moves to next row
             }
             Console.WriteLine("Above you see tik tak toe grid");
 
